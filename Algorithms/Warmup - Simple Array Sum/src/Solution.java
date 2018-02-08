8330f6164cdf7d315bd82e03b98c26df52f50dc1
Randle McMurphy <jmarton2000@gmail.com>
2018-02-08 16:33:12 +0200

    Add Warmup - Simple Array Sum

---
+++ Algorithms/Warmup - Simple Array Sum/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    static int simpleArraySum(int n, int[] ar) {
+        return IntStream.of(ar).sum();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        in.close();
+        int result = simpleArraySum(n, ar);
+        System.out.println(result);
+    }
+}
