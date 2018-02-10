57c3c79b4299735ccff2f91278bba461dcbe21e1
Randle McMurphy <jmarton2000@gmail.com>
2018-02-10 20:28:31 +0200

    Add Implementation - Divisible Sum Pairs

---
+++ Algorithms/Implementation - Divisible Sum Pairs/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+    	int count = 0;
+    	for (int i = 0; i < ar.length - 1; i++)
+    		for (int j = i + 1; j < ar.length; j++)
+    			if ((ar[i] + ar[j]) % k == 0)
+    				count++;
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        in.close();
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
+}
